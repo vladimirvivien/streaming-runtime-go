dc2cb03f0ebefef39264d7b4c602934fe4e150db
Vladimir Vivien <vivienv@vmware.com>
2022-04-06 14:00:31 -0400

    Adding new example for data center power utitilization; Starts support for Table

--- main.go
+++ main.go
@@ -120,6 +120,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Channel")
 		os.Exit(1)
 	}
+	if err = (&controllers.TableReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Table")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
